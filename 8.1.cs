7479868ae1f3e9fc69e8eecc6aeaa318c64033e0
Елизавета Боярко <LizOK_boarko@mail.ru>
2021-11-13 12:42:46 +0300

    Добавьте файлы проекта.

---
+++ 8.1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Задание_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:/Users/Elizaveta/Desktop/Автоматизация BIM";
+            Console.WriteLine("Содержимое основного каталога:");
+            string[] dirs = Directory.GetDirectories(path);
+            foreach (string s in dirs)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine();
+            foreach (string s in dirs)
+            {
+                string[] subdirs = Directory.GetDirectories(s);
+                Console.WriteLine();
+                Console.WriteLine($"Содержимое подкаталога: {subdirs}");
+                foreach (string a in subdirs)
+                {
+                    Console.WriteLine(a);
+                }
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Задание 8.2/8.2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Задание_8._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "File.txt";
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+            }
+            StreamWriter sw = new StreamWriter(path);
+            Random random = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                sw.WriteLine(random.Next(0, 10));
+            }
+            sw.Close();
+
+            int[] arraynumbers = new int[10];
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                for (int i = 0; i < arraynumbers.Length; i++)
+                {
+                    arraynumbers[i] = Convert.ToInt32(sr.ReadLine());
+                }
+            }
+            int Sum = 0;
+            for (int i = 0; i < arraynumbers.Length; i++)
+            {
+                Sum += arraynumbers[i];
+            }
+            Console.WriteLine(Sum);
+            Console.ReadKey();
+        }
+    }
+}
